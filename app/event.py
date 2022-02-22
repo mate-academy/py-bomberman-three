b123d6639351a11365bfb77bf6438022c1df3952
Ihor Olifirenko <igoroliphirenko@gmail.com>
2022-02-22 08:00:12 +0200

    Solution

--- app/event.py
+++ app/event.py
@@ -1,29 +1,37 @@
 import pygame
 import abc
 
-from sprites import Enemy
+from sprites import Spider, Boar, Bird
 
 from mixins import EngineMixin
 
 
 class Event(EngineMixin, abc.ABC):
-    def __init__(self, ms_timeout):
+    def __init__(self, ms_timeout=1000):
         super().__init__()
         self.ms_timeout = ms_timeout
-
-    @abc.abstractmethod
-    def action(self):
-        pass
-
-
-class AddEnemy(Event):
-    def __init__(self, ms_timeout=1000):
-        super().__init__(ms_timeout)
 
         self.event_no = pygame.USEREVENT + 1
         pygame.USEREVENT = self.event_no
         pygame.time.set_timer(self.event_no, self.ms_timeout)
         self.engine.add_event(self)
 
+    @abc.abstractmethod
     def action(self):
-        Enemy()
+        pass
+
+
+class AddSpider(Event):
+
+    def action(self):
+        Spider()
+
+
+class AddBoar(Event):
+    def action(self):
+        Boar()
+
+
+class AddBird(Event):
+    def action(self):
+        Bird()
